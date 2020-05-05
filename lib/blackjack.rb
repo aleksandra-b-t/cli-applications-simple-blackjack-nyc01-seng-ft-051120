640dab9070b8b8b471a25e6fd9fd916781823261
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-05-05 23:17:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,10 +18,9 @@ def prompt_user
   puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
+def get_user_input(a = gets.chomp)
   # code #get_user_input here
- gets.chomp
-
+ a
 end
 
 def end_game(number)
